3ce0b95863c26d504d405f8f03e96046290f6b26
codetime66 <carlosandrefernandes@gmail.com>
2017-06-06 23:45:02 -0300

    fixing config file

--- mongodb/runvol.sh
+++ mongodb/runvol.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -p 27017:27017 --name mongo_001 -v /home/codetime/projects/lab01/mongodb/datadir:/data/db -d mongodb:3.2
+sudo docker run -p 27017:27017 --name mongo_001 -v /home/codetime/projects/docking/mongodb/datadir:/data/db -d mongodb:3.2
